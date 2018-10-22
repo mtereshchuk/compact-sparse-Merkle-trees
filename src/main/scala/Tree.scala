1861b99ecdda2daaaf3e0ac5f047c0c77e98a548
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-22 23:53:42 +0300

    Change return value for getProof

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,3 +1,4 @@
+import model.adt.MembershipProof
 import model.utils.{Node, TreeUtils}
 
 class Tree {
@@ -5,7 +6,7 @@ class Tree {
 
   def insert(k: BigInt, v:String):Unit = if (this.root == null) root = TreeUtils.makeNode(k, v) else root = CSMT.insert(root, k, v)
 
-  def getProof(k:BigInt):Any = if (this.root == null) null else CSMT.getProof(root, k)
+  def getProof(k:BigInt):MembershipProof = if (this.root == null) null else CSMT.getProof(root, k)
 
   def delete(k:BigInt): Unit = if (this.root != null) CSMT.delete(root, k)
 }
