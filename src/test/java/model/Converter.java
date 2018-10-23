6b6ab6ffe4e7f177b9506839b7c050d8d58646de
sergeitelnow <sergeitelnow@gmail.com>
2018-10-23 23:31:52 +0300

    refactor TestUtils

--- src/test/java/model/Converter.java
+++ src/test/java/model/Converter.java
@@ -4,15 +4,17 @@ import com.google.common.base.Charsets;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class Converter {
 
     private static final byte[] ZERO = new byte[]{0b00000000};
     private static final byte[] ONE = new byte[]{0b00000001};
     private static final byte[] TWO = new byte[]{0b00000010};
-    private static final byte[] EMPTY_LINE = new byte[]{};
 
     private static MessageDigest digest;
+    private static Base64.Encoder encoderBase64 = Base64.getEncoder();
+    private static Base64.Decoder decoderBase64 = Base64.getDecoder();
 
     static {
         try {
@@ -42,15 +44,40 @@ public class Converter {
         }
     }
 
-    public static byte[] encode(byte[] bytes) {
+    public static String countLeafHash(String value) {
+        return encoderBase64.encodeToString(encode(combine(ZERO, value.getBytes())));
+    }
+
+    public static String countHash(String left, String right) {
+        if (left.equals("null") && !right.equals("null")) {
+            byte[] bytes = encode(combine(ONE, TWO, decoderBase64.decode(right)));
+            return encoderBase64.encodeToString(bytes);
+        } else if (!left.equals("null") && right.equals("null")) {
+            byte[] bytes = encode(combine(ONE, decoderBase64.decode(left), TWO));
+            return encoderBase64.encodeToString(bytes);
+        } else if (!left.equals("null") && !right.equals("null")) {
+            byte[] bytes = encode(combine(ONE, decoderBase64.decode(left), TWO, decoderBase64.decode(right)));
+            return encoderBase64.encodeToString(bytes);
+        }
+        return "null";
+    }
+
+    private static byte[] encode(byte[] bytes) {
         return digest.digest(bytes);
     }
 
-    private static byte[] combine(byte[] bytes1, byte[] bytes2) {
-        byte[] bytes = new byte[bytes1.length + bytes2.length];
-        System.arraycopy(bytes1, 0, bytes, 0, bytes1.length);
-        System.arraycopy(bytes2, 0, bytes, bytes1.length, bytes2.length);
-
-        return bytes;
+    private static byte[] combine(byte[]... bytes) {
+        int length = 0;
+        for (byte[] seg : bytes)
+            length += seg.length;
+        byte[] concatenated = new byte[length];
+        int index = 0;
+        for (byte[] seq : bytes) {
+            for (byte b : seq) {
+                concatenated[index] = b;
+                index++;
+            }
+        }
+        return concatenated;
     }
 }
--- src/test/java/model/TestUtils.java
+++ src/test/java/model/TestUtils.java
@@ -11,15 +11,11 @@ public final class TestUtils {
     public static final Function<Integer, Integer> SIMPLE_LEAF_FUNC = (it) -> it;
     public static final BinaryOperator<Integer> SIMPLE_NODE_FUNC = (a, b) -> a + b;
 
-    private static byte[] zero = {0b00000000};
-    private static byte[] one = {0b00000001};
-    private static byte[] two = {0b00000010};
-
-    private static Base64.Encoder encoder = Base64.getEncoder();
-    private static Base64.Decoder decoder = Base64.getDecoder();
-
     public static final Function<String, byte[]> LEAF_HASH_FUNCTION = Converter::encode;
     public static final BinaryOperator<byte[]> NODE_HASH_FUNCTION = Converter::encode;
+
+    public static final Function<String, String> SCALA_LEAF_FUNC = Converter::countLeafHash;
+    public static final BinaryOperator<String> SCALA_NODE_FUNC = Converter::countHash;
 
     public static int toPow2(final int value) {
         int highestOneBit = Integer.highestOneBit(value);
@@ -32,42 +28,5 @@ public final class TestUtils {
     public static int log2(final int x) {
         return (int) Math.ceil(Math.log(x) / Math.log(2));
     }
-
-
-    public static String countLeafHash(String value) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        return encoder.encodeToString(digest.digest(concatenateByteArrays(zero, value.getBytes())));
-    }
-
-    public static String countHash(String left, String right) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        if (left.equals("null") && !right.equals("null")) {
-            byte[] bytes = digest.digest(concatenateByteArrays(one, two, decoder.decode(right)));
-            return encoder.encodeToString(bytes);
-        } else if (!left.equals("null") && right.equals("null")) {
-            byte[] bytes = digest.digest(concatenateByteArrays(one, decoder.decode(left), two));
-            return encoder.encodeToString(bytes);
-        } else if (!left.equals("null") && !right.equals("null")) {
-            byte[] bytes = digest.digest(concatenateByteArrays(one, decoder.decode(left), two, decoder.decode(right)));
-            return encoder.encodeToString(bytes);
-        }
-        return "null";
-    }
-
-    private static byte[] concatenateByteArrays(byte[]... bytes) {
-        int length = 0;
-        for (byte[] seg : bytes)
-            length += seg.length;
-        byte[] concatenated = new byte[length];
-        int index = 0;
-        for (byte[] seq : bytes) {
-            for (byte b : seq) {
-                concatenated[index] = b;
-                index++;
-            }
-        }
-        return concatenated;
-    }
-
 
 }
