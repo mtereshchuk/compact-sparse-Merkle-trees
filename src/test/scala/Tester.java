7c9d513a57b87a8bfa17c11927c59e1f61359d65
sergeitelnow <sergeitelnow@gmail.com>
2018-10-24 01:39:07 +0300

    add scala simple test

--- src/test/scala/Tester.java
+++ src/test/scala/Tester.java
@@ -1,50 +1,41 @@
-import model.TestUtils;
-import model.adt.MembershipProof;
-import model.tester.TestFactory;
+import implementation.Tree;
+import implementation.adt.ProofResult;
 import org.junit.Test;
 import scala.math.BigInt;
 
-import java.math.BigInteger;
-import java.util.Iterator;
-
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class Tester {
 
     @Test
-    public void test() {
+    public void CSMTInsertTestScala() {
         Tree tree = new Tree();
-
-        TestFactory.Test test = TestFactory.getRandomMerkelTest(5);
-        for (int i = 0; i != test.input.length; i++) {
-            tree.insert(new BigInt(BigInteger.valueOf(i)), test.input[i]);
+        String[] input = {"abc", "cde", "efg", "ghi", "ijk"};
+        for (int i = 0; i != input.length; i++) {
+            tree.insert(BigInt.apply(i), input[i]);
         }
-
-
+        checkProof(tree, input.length);
     }
 
-    //    private void checkProofs(TestFactory.Test test, Tree tree) {
-//        final int high = TestUtils.log2(test.input.length);
-//
-//        for (int i = 0; i != test.input.length; i++) {
-//            MembershipProof proof = tree.getProof(new BigInt(BigInteger.valueOf(i)));
-//
-//            LeafNode leaf = proof.getNode();
-//
-//            assertArrayEquals(test.hashes[0][i], (byte[]) leaf.getHash());
-//
-//            int index = i;
-//            Iterator proofIterator = membershipProof.getProof().iterator();
-//            for (int h = 0; h != high; h++) {
-//                int currIndex = index % 2 == 0 ? index + 1 : index - 1;
-//                byte[] testHash = test.hashes[h][currIndex];
-//                if (testHash != null) {
-//                    byte[] currHash = (byte[]) ((MembershipProof.Entry) proofIterator.next()).getHash();
-//                    assertArrayEquals("at index=" + i + " at level: " + h, testHash, currHash);
-//                }
-//                index /= 2;
-//            }
-//        }
-//    }
+    private void checkProof(Tree tree, final int numberOfElements) {
+        for (int i = 0; i != numberOfElements; i++) {
+            implementation.adt.MembershipProof proof = tree.getProof(BigInt.apply(i));
+            ProofResult membershipProof = (ProofResult) proof;
 
+            assertNotNull(proof);
+            assertNotNull(membershipProof);
+//                int index = i;
+//                Iterator proofIterator = membershipProof.proof().iterator();
+//                for (int h = 0; h != high; h++) {
+//                    int currIndex = index % 2 == 0 ? index + 1 : index - 1;
+//                    byte[] testHash = test.hashes[h][currIndex];
+//                    if (testHash != null) {
+//                        byte[] currHash = (byte[]) ((Tuple2<String, String>) proofIterator.next())._1.getBytes();
+//                        assertArrayEquals("at index=" + i + " at level: " + h, testHash, currHash);
+//                    }
+//                    index /= 2;
+//                }
+        }
+    }
 }
+
